4bc2ac205793dea7053fe6f6992c06d8793ebb74
laura Whellans <laurawhellans@lauras-iMac.local>
2015-05-23 22:40:05 +1000

    Created a robot object with a move function that will move the robot based on its current facing direction. Bounds for the grid are checked, to ensure that the robot does move past the maximum constraints set for the grid.
    Added Tests for movement.

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -1,5 +1,108 @@
-describe("Hello world", function() {
-    it("says hello", function() {
-        expect(helloWorld()).toEqual("Hello world!");
+/**
+ * Author: Laura Whellans
+ */
+
+describe("Move", function() {
+    describe("East", function() {
+
+        beforeEach(function() {
+            robot.facing = "EAST";
+            robot.x = 0;
+            robot.y = 0;
+        });
+
+        it("should increment x by 1", function() {
+            robot.move();
+            expect(robot.x).toEqual(1);
+        });
+
+        it("should not increment past the grid width of " + grid.width, function() {
+            // expect robot.x == 0
+            robot.move(); // expect robot.x == 1
+            robot.move(); // expect robot.x == 2
+            robot.move(); // expect robot.x == 3
+            robot.move(); // expect robot.x == 4
+            robot.move(); // expect robot.x to not increase after 4
+            robot.move(); // expect robot.x to not increase after 4
+            expect(robot.x).toEqual(grid.width);
+        });
+
+
+    });
+
+    describe("West", function() {
+
+        beforeEach(function() {
+            robot.facing = "WEST";
+            robot.x = 4;
+            robot.y = 4;
+        });
+
+        it("should decrement x by 1", function() {
+            robot.move();
+            expect(robot.x).toEqual(3);
+        });
+
+        it("should not decrement below 0", function() {
+            // expect robot.x == 4
+            robot.move(); // expect robot.x == 3
+            robot.move(); // expect robot.x == 2
+            robot.move(); // expect robot.x == 1
+            robot.move(); // expect robot.x == 0
+            robot.move(); // expect robot.x to not decrease less than 0
+            robot.move(); // expect robot.x to not decrease less than 0
+            expect(robot.x).toEqual(0);
+        });
+    });
+
+    describe("North", function() {
+
+        beforeEach(function() {
+            robot.facing = "NORTH";
+            robot.x = 0;
+            robot.y = 0;
+        });
+
+        it("should increment y by 1", function() {
+            robot.move();
+            expect(robot.y).toEqual(1);
+        });
+
+        it("should not increment past the grid width of " + grid.height, function() {
+            // expect robot.y == 0
+            robot.move(); // expect robot.y == 1
+            robot.move(); // expect robot.y == 2
+            robot.move(); // expect robot.y == 3
+            robot.move(); // expect robot.y == 4
+            robot.move(); // expect robot.y to not increase after 4
+            robot.move(); // expect robot.y to not increase after 4
+            expect(robot.y).toEqual(grid.height);
+        });
+    });
+
+
+    describe("South", function() {
+
+        beforeEach(function() {
+            robot.facing = "SOUTH";
+            robot.x = 4;
+            robot.y = 4;
+        });
+
+        it("should decrement y by 1", function() {
+            robot.move();
+            expect(robot.y).toEqual(3);
+        });
+
+        it("should not decrement below 0", function() {
+            // expect robot.y == 4
+            robot.move(); // expect robot.y == 3
+            robot.move(); // expect robot.y == 2
+            robot.move(); // expect robot.y == 1
+            robot.move(); // expect robot.y == 0
+            robot.move(); // expect robot.y to not decrease less than 0
+            robot.move(); // expect robot.y to not decrease less than 0
+            expect(robot.y).toEqual(0);
+        });
     });
 });
--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
-function helloWorld() {
-    return "Hello world!";
-}
+/**
+ * Author: Laura Whellans
+ */
+
+//  [4, 0] | [4, 1] | [4, 2] | [4, 3] | [4, 4]
+//
+//  [3, 0] | [3, 1] | [3, 2] | [3, 3] | [3, 4]
+//
+//  [2, 0] | [2, 1] | [2, 2] | [2, 3] | [2, 4]
+//
+//  [1, 0] | [1, 1] | [1, 2] | [1, 3] | [1, 4]
+//
+//  [0, 0] | [0, 1] | [0, 2] | [0, 3] | [0, 4]
+
+//        N
+//        ↑
+//        |
+//  W <---O---> E
+//        |
+//        ↓
+//        S
+
+/**
+ * The Grid Constraints.
+ * @type {{width: number, height: number}}
+ */
+var grid = {
+    width: 4,
+    height: 4
+};
+
+/**
+ * The robot object to be moved within the grid.
+ * @type {{facing: string, x: number, y: number, move: Function}}
+ */
+var robot = {
+    facing: "NORTH",
+    x: 0,
+    y: 0,
+    move: function() {
+        move[this.facing]();
+    }
+};
+
+/**
+ * Checks the grid boundaries, and moves the robot by 1 in the current facing direction.
+ * @type {{
+ * NORTH: (function(this:{facing: string, x: number, y: number, move: Function})),
+ * SOUTH: (function(this:{facing: string, x: number, y: number, move: Function})),
+ * EAST: (function(this:{facing: string, x: number, y: number, move: Function})),
+ * WEST: (function(this:{facing: string, x: number, y: number, move: Function}))}}
+ */
+var move = {
+    NORTH: function () { if (this.y < grid.height) this.y++;}.bind(robot),
+    SOUTH: function () { if (this.y > 0) this.y--; }.bind(robot),
+    EAST: function () { if (this.x < grid.width) this.x++; }.bind(robot),
+    WEST: function () { if (this.x > 0) this.x--; }.bind(robot)
+};
